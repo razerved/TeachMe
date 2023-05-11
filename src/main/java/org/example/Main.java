7aa3fa541dbff5bb7e3fe3aa0454b98ab75f2ffd
IvanMedvedev <idmedvedev@vtb.ru>
2023-05-12 00:23:17 +0300

    newHomeWork5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,35 @@
 package org.example;
 
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //searchScannerLoop(scanner());
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+
+    }
+
+    private static Scanner scanner(){
+        return new Scanner(System.in);
+    }
+
+    private static void searchScannerLoop(Scanner scanner){
+        System.out.print("Please enter what number you want find? ");
+        int[] newArray = { 1, 2, 3, 4, 5 };
+        for (int i : newArray){
+            if (scanner().nextInt() == i) {
+                System.out.println("Yes, we found your number in this Array: " + i);
+            }else {
+                System.out.println("No in this Array don't this number ");
+            }
+            break;
         }
     }
+
+
+
 }
