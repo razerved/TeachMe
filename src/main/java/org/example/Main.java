82ed71858fe8366db49d194d3be170a314eb36bd
IvanMedvedev <idmedvedev@vtb.ru>
2023-05-12 00:23:17 +0300

    newHomeWork5
    newHomeWork5
    
    newHomeWork5
    
    newHomeWork5
    
    newHomeWork5
    
    newHomeWork5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,174 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //searchScannerLoop(scanner());
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        /*int[] b = {2,3,4,5,6};
+        System.out.println("put your number for search in array");
+        searchArrayNumb(b, scanner().nextInt());*/
+
+        sortedBubble();
+        //createNewArrayWithoutScanNumber(scanner());
+        //createArrayWithScan(scanner());
+        //differenceBetweenArray();
+
+    }
+
+    private static Scanner scanner(){
+        return new Scanner(System.in);
+    }
+
+    private static void searchScannerLoop(Scanner scanner){
+        System.out.print("Please enter what number you want find? ");
+        int[] newArray = { 1, 2, 3, 4, 5 };
+        int b = scanner().nextInt();
+        for (int i : newArray){
+            if ( b == i ) {
+                System.out.println("Yes, we found your number in this Array: " );
+                return;
+            }
+        }
+        System.out.println("No in this Array don't this number ");
+    }
+
+    private static void searchArrayNumb(int[] array, int number){
+        for (int i : array){
+            if ( number == i ){
+                System.out.println("yes");
+                return;
+            }
+        }
+        System.out.println("no");
+    }
+
+
+
+    private static void createNewArrayWithoutScanNumber(Scanner scanner){
+        int[] array = { 1, 4, 3, 8, 5 };
+        int a = scanner().nextInt();
+        int count = 0;
+        for (int q : array){
+            if (q == a){
+                count++;
+            }
+        }
+        if (count == 0){
+            System.out.println("D'oh");
+            return;
+        }
+        int[] finalArray = new int[array.length - count];
+        int index = 0;
+        for (int x = 0; x < array.length; x++){
+
+            if (array[x] != a){
+                finalArray[index++] = array[x];
+            }
+
+        }
+        for (int o : finalArray) {
+            System.out.print( o + " " );
         }
     }
+
+
+
+    private static void createArrayWithScan(Scanner scanner){
+        int[] array = new int[scanner().nextInt()];
+        int x = 278;
+
+        for (int i = 0; i < array.length; i++){
+            array[i] = (int)(Math.random() * x );
+            System.out.print( array[i] + " " );
+
+        }
+        /*int min = array[0];
+        int max = array[0];
+        int sum = array[0];
+        for (int i = 1; i < array.length; i++ ){
+            if (array[i] < min){
+                min = array[i];
+            }
+            if (array[i] > max){
+                max = array[i];
+            }
+            sum = sum + array[i];
+        }
+        System.out.println();
+        System.out.println( "Мах: " + max );
+        System.out.println( "Average: " + sum/array.length );
+        System.out.println( "Мин: " + min );*/
+
+
+        System.out.println();
+        System.out.println( "Мах: " + Arrays.stream(array).max() );
+        System.out.println( "Average: " + Arrays.stream(array).average());
+        System.out.println( "Мин: " + Arrays.stream(array).min() );
+    }
+
+
+    private static void differenceBetweenArray() {
+        int[] array = {1, 2, 3, 4, 5}; //3
+        int[] array2 = {5, 6, 7, 8, 9}; //7
+        int sumArray1 = 0;
+        int sumArray2 = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+            sumArray1 = array[i] + sumArray1;
+
+        }
+
+        System.out.println();
+        System.out.print("this averageArray1: " + sumArray1/array.length);
+        System.out.println();
+
+        for (int x = 0; x < array2.length; x++) {
+            System.out.print(array2[x] + " ");
+            sumArray2 = array2[x] + sumArray2;
+        }
+
+        System.out.println();
+        System.out.println("this averageArray2: " + sumArray2/array2.length);
+
+        if (sumArray1 > sumArray2) {
+            System.out.println("First Array bigger ");
+        } else if (sumArray1 < sumArray2) {
+            System.out.println("Second Array bigger");
+        } else
+            System.out.println(" First Array equals Second Array ");
+    }
+
+
+
+    private static void sortedBubble(){
+        int[] array = { 300, 163, 184, 191, 174 };
+
+        int lef = 0;
+        boolean isSorted = false;
+
+        while (!isSorted) {
+            isSorted = true;
+
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    lef = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = lef;
+                    isSorted = false;
+                }
+            }
+        }
+        for (int o : array){
+            System.out.println(o + " ");
+        }
+    }
+
+
 }
