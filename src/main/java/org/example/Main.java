9fb167cbcc6a3059400cf1cbc7b102490a139278
IvanMedvedev <idmedvedev@vtb.ru>
2023-05-13 02:17:42 +0300

    newHomeWork5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import javax.sound.midi.Soundbank;
+import java.util.Arrays;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -9,10 +9,14 @@ public class Main {
     public static void main(String[] args) {
 
         //searchScannerLoop(scanner());
-        int[] b = {2,3,4,5,6};
-        System.out.println("put your number for seaech in array");
-        searchArrayNumb(b, scanner().nextInt());
 
+        /*int[] b = {2,3,4,5,6};
+        System.out.println("put your number for search in array");
+        searchArrayNumb(b, scanner().nextInt());*/
+
+
+        //createNewArrayWithoutScanNumber(scanner());
+        createArrayWithScan(scanner());
 
     }
 
@@ -41,6 +45,43 @@ public class Main {
             }
         }
         System.out.println("no");
+    }
+
+
+
+    private static void createNewArrayWithoutScanNumber(Scanner scanner){
+        int[] array = { 1, 4, 3, 8, 5 };
+        int[] finalArray = new int[5];
+        int a = scanner().nextInt();
+        for (int x = 0; x < array.length; x++){
+
+            if (a == array[x]){
+                continue;
+            }
+
+            finalArray[x] = array[x];
+        }
+        // TODO придумать отбивку в случае если в массиве " такого числа нет - выведите сообщения об этом "
+        for (int o = 0; o < finalArray.length; o++) {
+            System.out.println( finalArray[o] );
+        }
+    }
+
+
+
+    private static void createArrayWithScan(Scanner scanner){
+        int[] array = new int[scanner().nextInt()];
+        int x = 278;
+        for (int i = 0; i < array.length; i++ ){
+            array[i] = (int)(Math.random() * x );
+        }
+        System.out.println( "Мах: " + Arrays.stream(array).max() );
+        System.out.println( "Мин: " + Arrays.stream(array).min() );
+    }
+
+
+    private static void differenceBetweenArray(){
+
     }
 
 }
