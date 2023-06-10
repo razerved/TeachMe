0c72749b6c15e4fd8a0d9460a716e7eb3f0034c4
IvanMedvedev <idmedvedev@vtb.ru>
2023-06-11 01:15:25 +0300

    RefHomework11

--- src/main/java/org/example/collectionClass/HashSetClass.java
+++ src/main/java/org/example/collectionClass/HashSetClass.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class HashSetClass {
     public static void main(String[] args) {
-        // getHashSet();
+        getHashSet();
         getLinkedHashSet();
     }
 
